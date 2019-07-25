be2ec6e98271e7945a21442beb9eaab9cd786499
Perry van den Hondel <perry.vandenhondel@gmail.com>
2019-07-25 12:10:29 +0200

    Updated utextfile-test

---
+++ utextfile-test.pas
@@ -0,0 +1,33 @@
+program UTextFileTest;
+
+
+uses
+  SysUtils,
+  UTextFile;
+
+
+var
+	tf: CTextFile;
+  tfr: CTextFile;
+  
+  
+begin
+	tf := CTextFile.Create('utexttest.txt');
+	tf.OpenFileWrite();
+	Writeln('The status of ' + tf.GetPath + ' is ' + BoolToStr(tf.GetStatus, 'OPEN', 'CLOSED'));
+  tf.WriteToFile('test text');
+  tf.WriteToFile('test text2');
+  tf.WriteToFile('testline');
+  
+  tf.CloseFile();
+  
+  tfr := CTextFile.Create('utexttest.txt');
+  tfr.OpenFileRead();
+  Writeln('The status of ' + tfr.GetPath + ' is ' + BoolToStr(tfr.GetStatus, 'OPEN', 'CLOSED'));
+  repeat
+    WriteLn(IntToStr(tfr.GetCurrentLine()) + ': ' + tfr.ReadFromFile());
+    
+  until tfr.GetEof();
+  
+  tfr.CloseFile();
+end.
