452948ac6f5bdb10dc51b8242b8ef74452938c90
Perry van den Hondel <perry.vandenhondel@gmail.com>
2019-12-10 20:14:55 +0100

    Updated testit for using new names

--- testit.pas
+++ testit.pas
@@ -16,17 +16,17 @@ var
 begin
   path := '/tmp/utextfile/test.log';
 
-	tf := CTextFile.Create(path);
-	tf.OpenFileWrite();
+	tf := CTextFile.CreateTheFile(path);
+	tf.OpenFileForWrite();
 	Writeln('The status of ' + tf.GetPath + ' is ' + BoolToStr(tf.GetStatus, 'OPEN', 'CLOSED'));
   tf.WriteToFile('test text');
   tf.WriteToFile('test text2');
   tf.WriteToFile('testline');
   
-  tf.CloseFile();
+  tf.CloseTheFile();
   
-  tfr := CTextFile.Create(path);
-  tfr.OpenFileRead();
+  tfr := CTextFile.CreateTheFile(path);
+  tfr.OpenFileForRead();
   Writeln('The status of ' + tfr.GetPath + ' is ' + BoolToStr(tfr.GetStatus, 'OPEN', 'CLOSED'));
   repeat
     WriteLn(IntToStr(tfr.GetLineNumber()) + ': ' + tfr.ReadFromFile());
@@ -34,14 +34,14 @@ begin
   
   //WriteLn('Size of ', tfr.GetPath, ' is ', tfr.GetFileSize());
 
-  tfr.CloseFile();
+  tfr.CloseTheFile();
 
 
   path := '/tmp/firstname.log';
-  tf := CTextFile.Create(path);
-  tf.OpenFileWrite();
+  tf := CTextFile.CreateTheFile(path);
+  tf.OpenFileForWrite();
   tf.WriteToFile('firstname line');
-  tf.CloseFile();
+  tf.CloseTheFile();
   tf.RenameTheFile('newname.log');
 
 
